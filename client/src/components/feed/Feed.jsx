364c57b21632c4728f7c013fc1bfd8e5008b52b6
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2023-02-08 19:10:08 -0500

    Finished adding left and feed displays, now working on right sidebar

---
+++ client/src/components/feed/Feed.jsx
@@ -0,0 +1,22 @@
+import "./feed.css";
+
+import "./feed.css";
+import Share from "../share/Share";
+import Post from "../post/Post";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
--- client/src/pages/home/Home.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Topbar from "../../components/topbar/Topbar";
-
-export const Home = () => {
-  return <Topbar />;
-};
