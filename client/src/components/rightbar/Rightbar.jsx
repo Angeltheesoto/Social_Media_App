2ea79bda9974d0f6e9517d8abb53cdd476972207
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2023-02-17 18:18:33 -0500

    Finished fixing follow and unfollow functionality

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -10,12 +10,14 @@ import { Add, Remove } from "@material-ui/icons";
 function Rightbar({ user }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const [friends, setFriends] = useState([]);
-  const { user: currentUser } = useContext(AuthContext);
-  const [followed, setFollowed] = useState(false);
+  const { user: currentUser, dispatch } = useContext(AuthContext);
+  const [followed, setFollowed] = useState(
+    currentUser.following.includes(user?.id)
+  );
 
-  useEffect(() => {
-    setFollowed(currentUser.following.includes(user?.id));
-  }, [currentUser, user.id]);
+  // useEffect(() => {
+  //   setFollowed(currentUser.following.includes(user?.id));
+  // }, [currentUser, user.id]);
 
   useEffect(() => {
     const getFriends = async () => {
@@ -32,13 +34,15 @@ function Rightbar({ user }) {
   const handleClick = async () => {
     try {
       if (followed) {
-        await axios.put("/api/users/" + user._id + "/follow", {
-          userId: currentUser._id,
-        });
-      } else {
         await axios.put("/api/users/" + user._id + "/unfollow", {
           userId: currentUser._id,
         });
+        dispatch({ type: "UNFOLLOW", payload: user._id });
+      } else {
+        await axios.put("/api/users/" + user._id + "/follow", {
+          userId: currentUser._id,
+        });
+        dispatch({ type: "FOLLOW", payload: user._id });
       }
       setFollowed(!followed);
     } catch (err) {
